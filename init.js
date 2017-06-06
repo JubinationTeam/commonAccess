60bcc0abe49f58a8cbd4eb5a14dffdd333aa90fc
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-06 15:40:19 +0530

    msg

--- init.js
+++ init.js
@@ -52,7 +52,7 @@ var dataAccessInitModel={
     };
 
 const urls={
-    'guard':'https://mysterious-garden-59970.herokuapp.com/',
+    'guard':'https://mysterious-garden-59970.herokuapp.com',
     'aws':'http://139.59.39.95',
     'parser':'http://35.154.233.231/Parser',
     'vendor':{
